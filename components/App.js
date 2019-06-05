f43e30faf831d8f6dd22d31a68e02dbaad302516
Marcin <marcin.aleksanderek@gmail.com>
2019-06-05 22:59:03 +0200

    Update to premisses

--- components/App.js
+++ components/App.js
@@ -17,30 +17,39 @@ App = React.createClass({
 		this.setState({
 			loading: true
 		});
-		this.getGif(searchingText, function(gif) {
+		this.getGif(searchingText).then(gif =>{
 			this.setState({
 			loading: false,
 			gif: gif,
 			searchingText: searchingText
 			});
-		}.bind(this));
+		});
 	},
 
 	getGif: function(searchingText, callback) {
 		var url = GIPHY_API_URL + 'gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText + '&limit=25&offset=0&rating=G&lang=en';
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', url);
-		xhr.onload = function() {
-				if (xhr.status === 200) {
-					 var data = JSON.parse(xhr.responseText).data;
+		return new Promise(
+			function(resolve, reject) {
+				const request = new XMLHttpRequest();
+				request.onload = function() {
+					if (this.status === 200) {
+						var data = JSON.parse(request.responseText).data;
 						var gif = {
 								url: data.fixed_width_downsampled_url,
 								sourceUrl: data.url
 						};
-						callback(gif);
-				}
-		};
-		xhr.send();
+						resolve(gif);
+					} else {
+						reject(new Error(this.statusText));
+					}
+				};
+				request.onerror = function() {
+					reject(new Error(
+						`XMLHttpRequest Error: ${this.statusText}`));
+				};
+				request.open('GET', url);
+				request.send();
+			});
 	},
 
 	render: function() {
